7150a6172a877ca6ccef73c52d41b8b0fbd48b5b
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-11-22 21:53:51 +0530

    Implement sorting and handle card image

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
+    sortOrder: string;
 }
 
 const App = () => {
@@ -47,7 +48,12 @@ const App = () => {
                         }
                         selectedPlatform={gameQuery.platform}
                     />
-                    <SortSelector />
+                    <SortSelector
+                        onSelectSortOrder={(sortOrder) =>
+                            setGameQuery({ ...gameQuery, sortOrder })
+                        }
+                        selectedSortOrder={gameQuery.sortOrder}
+                    />
                 </HStack>
                 <GameGrid gameQuery={gameQuery} />
             </GridItem>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,16 +1,56 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+    onSelectSortOrder: (sortOrder: string) => void;
+    selectedSortOrder: string;
+}
+
+const sortOrders = [
+    {
+        value: "",
+        label: "Relevance",
+    },
+    {
+        value: "-added",
+        label: "Date added",
+    },
+    {
+        value: "name",
+        label: "Name",
+    },
+    {
+        value: "-released",
+        label: "Release date",
+    },
+    {
+        value: "-metacritic",
+        label: "Popularity",
+    },
+    {
+        value: "-rating",
+        label: "Average rating",
+    },
+];
+
+const SortSelector = ({ onSelectSortOrder, selectedSortOrder }: Props) => {
+    const currentSortOrder = sortOrders.find(
+        (order) => order.value === selectedSortOrder
+    )?.label;
     return (
         <Menu>
             <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-                Sort Game
+                Sort by: {currentSortOrder || "Relevance"}
             </MenuButton>
             <MenuList>
-                <MenuItem>Item 1</MenuItem>
-                <MenuItem>Item 2</MenuItem>
-                <MenuItem>Item 3</MenuItem>
+                {sortOrders.map((order) => (
+                    <MenuItem
+                        onClick={() => onSelectSortOrder(order.value)}
+                        key={order.value}
+                    >
+                        {order.label}
+                    </MenuItem>
+                ))}
             </MenuList>
         </Menu>
     );
